a2620d791759885edb1861bae73a34d31ecddefb
Wilgrey Ravelo Cruz <167430214+Wilgrey2004@users.noreply.github.com>
2024-11-13 22:00:26 -0400

    avance de hoy

---
+++ server/index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import logger from "morgan";
+
+import { Server } from "socket.io";
+import { createServer } from "node:http";
+
+const port = process.env.PORT ?? 3000;
+
+const app = express();
+
+const server = createServer(app);
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log("a user has connected");
+
+  socket.on("disconnect", () => {
+    console.log("an user has disconnect");
+  });
+
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+  });
+});
+
+app.use(logger("dev"));
+
+app.get("/", (req, res) => {
+  res.sendFile(process.cwd() + "/cliente/index.html");
+});
+
+server.listen(port, () => {
+  console.log("Server is runing in the port " + port);
+});
